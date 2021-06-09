8f9e0be69c7da6c595c64afe498f93e9d8eb7915
nsne <15527632169@163.com>
2021-06-09 16:14:19 +0800

    feat: 🎸 debug hooks state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-import * as React from 'react';
-import { Component, Suspense, Fragment } from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
+const App = () => {
+  const [count, setCount] = React.useState(0);
 
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'));
+  return (
+    <div>
+      <p>count: {count}</p>
+      <button
+        onClick={() => {
+          debugger;
+          setCount(count + 1);
+        }}
+      >
+        count + 1
+      </button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 module.hot && module.hot.accept();
